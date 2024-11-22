e71b12277944900eabb998278eeea7b3bdf47f85
d <132237963+conkeh@users.noreply.github.com>
2024-11-22 10:50:04 -0500

    update

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
+// navbar------------------------------------------------------------------------------->
+function Menu(e) {
+  let list = document.querySelector("ul");
+  e.name === "menu"
+    ? ((e.name = "close"),
+      list.classList.add("top-[80px]"),
+      list.classList.add("opacity-100"))
+    : ((e.name = "menu"),
+      list.classList.remove("top-[80px]"),
+      list.classList.remove("opacity-100"));
+}
+
+// gallery carousel------------------------------------------------------------------->
 const carousel = document.querySelector(".carousel");
 const arrowBtns = document.querySelectorAll(".wrapper i");
 const firstCardWidth = carousel.querySelector(".cards").offsetWidth;
